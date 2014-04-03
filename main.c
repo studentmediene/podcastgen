14ff991ae92b2113ab392d300f94eb0c9f046c50
Trygve Bertelsen Wiig <trygvebw@gmail.com>
2014-04-03 19:41:44 +0200

    Moved some things into separate functions

--- main.c
+++ main.c
@@ -15,62 +15,53 @@
 
 typedef enum { false, true } bool;
 
+SNDFILE *source_file;
+SF_INFO source_info;
+
+SNDFILE *dest_file;
+SF_INFO dest_info;
+
+int RMS_FRAME_LENGTH = 20; // Length of the RMS calculation frames in milliseconds
+int FRAMES_IN_RMS_FRAME;
+int RMS_FRAME_COUNT;
+
 int signum(float n) {
 	return (n > 0) - (n < 0);
 }
 
-int main(int argc, char *argv[]) {
-	// GET FILE TO OPEN FROM ARGUMENT
-	const char* filename;
-	if (argc > 1) {
-		filename = argv[1];
-	} else {
-		printf("Please supply a source file.\n");
-		return 1;
-	}
-
-	// SOURCE FILE
-	SNDFILE *source_file;
-	SF_INFO source_info;
-
+void open_source_file(const char *filename) {
 	if (!(source_file = sf_open(filename, SFM_READ, &source_info))) {
 		printf("An error occured while opening the source file.\n");
 		printf("%s\n", sf_strerror(NULL));
-		return 1;
+		exit(1);
 	}
+}
 
-	// DESTINATION FILE
-	SNDFILE *dest_file;
-	SF_INFO dest_info;
-	dest_info.samplerate = 44100;
-	dest_info.channels = 2;
-	dest_info.format = SF_FORMAT_WAV ^ SF_FORMAT_PCM_16 ^ SF_ENDIAN_FILE;
-
+void open_dest_file() {
 	if (!(dest_file = sf_open("testsound_out.wav", SFM_WRITE, &dest_info))) {
 		printf("An error occured while opening the destination file.\n");
 		printf("%s\n", sf_strerror(NULL));
-		return 1;
+		exit(1);
 	}
+}
 
-	// SOURCE FILE INFO
-	printf("Sample rate: %d\n", source_info.samplerate);
-	printf("Frames: %d\n", (int) source_info.frames);
-	printf("Channels: %d\n", source_info.channels);
-	printf("%f minutes long.\n", (source_info.frames/(double) source_info.samplerate)/60.0);
+char *interpret_args(int argc, char *argv[]) {
+	if (argc > 1) {
+		return argv[1];
+	} else {
+		printf("Please supply a source file.\n");
+		exit(1);
+	}
+}
 
-	// CALCULATE RMS
-
-	int RMS_FRAME_LENGTH = 20; // milliseconds
-	int FRAMES_IN_RMS_FRAME = (source_info.samplerate*RMS_FRAME_LENGTH)/1000; // (44100/1000)*20
-	int RMS_FRAME_COUNT = ceil(source_info.frames/(float) FRAMES_IN_RMS_FRAME);
-	
-	float *rms = malloc(2*RMS_FRAME_COUNT*sizeof(float));
+float *calculate_rms(float *rms) {
 	float *read_cache = malloc(2*FRAMES_IN_RMS_FRAME*sizeof(float));
 	sf_count_t frames_read = 0;
 
 	int frame; // dummy variable for inner loop
 	double local_rms; // temporary variable for inner loop
 
+	printf("%d\n", RMS_FRAME_COUNT);
 	for (int rms_frame = 0; rms_frame < RMS_FRAME_COUNT; rms_frame++) {
 		frames_read = sf_readf_float(source_file, read_cache, FRAMES_IN_RMS_FRAME);
 
@@ -82,6 +73,38 @@ int main(int argc, char *argv[]) {
 
 		rms[rms_frame] = local_rms;
 	}
+
+	free(read_cache);
+
+	return rms;
+}
+
+int main(int argc, char *argv[]) {
+	// GET FILE TO OPEN FROM ARGUMENT
+	const char *filename = interpret_args(argc, argv);
+
+	// SOURCE FILE
+	open_source_file(filename);
+
+	// DESTINATION FILE
+	dest_info.samplerate = 44100;
+	dest_info.channels = 2;
+	dest_info.format = SF_FORMAT_WAV ^ SF_FORMAT_PCM_16 ^ SF_ENDIAN_FILE;
+
+	open_dest_file(dest_file, &dest_info);
+
+	// SOURCE FILE INFO
+	printf("Sample rate: %d\n", source_info.samplerate);
+	printf("Frames: %d\n", (int) source_info.frames);
+	printf("Channels: %d\n", source_info.channels);
+	printf("%f minutes long.\n", (source_info.frames/(double) source_info.samplerate)/60.0);
+
+	// CALCULATE RMS
+
+	FRAMES_IN_RMS_FRAME = (source_info.samplerate*RMS_FRAME_LENGTH)/1000; // (44100/1000)*20
+	RMS_FRAME_COUNT = ceil(source_info.frames/(float) FRAMES_IN_RMS_FRAME);
+	float *rms = malloc(2*RMS_FRAME_COUNT*sizeof(float));
+	calculate_rms(rms);
 
 	puts("Calculated RMS...");
 
@@ -157,6 +180,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+
 	// Second pass: check if there are other speech segments within the next 10 frames
 	music_second_pass[0] = true;
 	music_second_pass[1] = true;
@@ -164,13 +188,6 @@ int main(int argc, char *argv[]) {
 		music_second_pass[long_frame] = rint((music[long_frame-2]+music[long_frame-1]+music[long_frame]+music[long_frame+1]+music[long_frame+2])/5.0);
 	}
 
-	/*for (int long_frame = 0; long_frame < LONG_FRAME_COUNT-10; long_frame++) {
-		if (music_second_pass[long_frame]) {
-			puts("MUSIC");
-		} else {
-			puts("-----");
-		}
-	}*/
 	free(music);
 
 	// Third pass: merge smaller segments with larger segments
@@ -227,12 +244,10 @@ int main(int argc, char *argv[]) {
 	int frames_to_read;
 	sf_count_t current_read_location;
 	sf_count_t current_write_location;
-	puts("Writing...");
 	for (int s = 0; s < current_merged_segment; s++) {
 		if (merged_segments[s].is_music) {
 			continue;
 		} else {
-			puts("Seeking...");
 			if (s == 0) {
 				current_read_location = sf_seek(source_file, (merged_segments[s].startframe)*FRAMES_IN_LONG_FRAME, SEEK_SET);
 				frames_to_read = (merged_segments[s].endframe-merged_segments[s].startframe+5)*FRAMES_IN_LONG_FRAME;
@@ -240,15 +255,10 @@ int main(int argc, char *argv[]) {
 				current_read_location = sf_seek(source_file, (merged_segments[s].startframe-5)*FRAMES_IN_LONG_FRAME, SEEK_SET);
 				frames_to_read = (merged_segments[s].endframe-merged_segments[s].startframe+10)*FRAMES_IN_LONG_FRAME;
 			}
-			puts("Allocating...");
 			cache = realloc(cache, frames_to_read*2*sizeof(float));
-			puts("Reading...");
 			current_read_location = sf_readf_float(source_file, cache, frames_to_read);
-			puts("Writing...");
 			current_write_location = sf_writef_float(dest_file, cache, frames_to_read);
-			puts("Syncing...");
 			sf_write_sync(dest_file);
-			puts("Written!");
 		}
 	}
 
@@ -258,7 +268,6 @@ int main(int argc, char *argv[]) {
 	int dest_file_status = sf_close(dest_file);
 
 	free(rms);
-	free(read_cache);
 
 	return source_file_status && dest_file_status;
 }
